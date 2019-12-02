9fee34ab37b4f6c6e34cf0016f13e3806fb40079
Rebecca Douglass <beccadouglass@yahoo.com>
2019-12-02 23:57:14 +0000

    changes to order controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_202115) do
+ActiveRecord::Schema.define(version: 2019_12_02_233801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "order_items", force: :cascade do |t|
     t.integer "quantity", default: 0
-    t.decimal "price", precision: 15, scale: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "order_id", null: false
